fa00c69d15f77846ac3efe52d484c17ef3673e89
Yam Sol Britto Bertamini <--get>
2022-06-03 11:38:07 -0300

    Alterações: - Ordem <?php ?> e <html> - merge com branch cookie_*

--- adm/inserir_aluno.php
+++ adm/inserir_aluno.php
@@ -1,41 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <script>
-        function Cadastro(_nome) {
-            this.Nome = _nome;
-            this.Cpf = _cpf;
-            this.Senha = _senha;
-            this.Data_Nasc = _data_nasc;
-            this.Endereco = _endereco;
-            this.Cep = _cep;
-            this.Bairro = _bairro;
-            this.Cidade = _cidade;
-            this.Uf = _uf;
-            this.Email = _email;
-        }
-        cadastro = new Cadastro(
-            <?php echo $nome ?>,
-            <?php echo $cpf ?>,
-            <?php echo $senha ?>,
-            <?php echo $data_nasc ?>,
-            <?php echo $endereco ?>,
-            <?php echo $cep ?>,
-            <?php echo $bairro ?>,
-            <?php echo $cidade ?>,
-            <?php echo $uf ?>,
-            <?php echo $email ?>,
-        )
-
-        console.log("My name is: " + cadastro.Nome + " :D")
-    </script>
-</head>
-
-<body></body>
-
-</html>
-
 <?php
 // importar arquivo de conexão
 include_once "conexao.php";
@@ -65,8 +27,48 @@ $_->bindValue(":bairro", $bairro);
 $_->bindValue(":cidade", $cidade);
 $_->bindValue(":uf", $uf);
 $_->bindValue(":email", $email);
-$_->execute();
+//$_->execute();
 
 // header("Location: ../");
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script>
+        function Cadastro(_nome, _cpf, _senha, _data_nasc, _endereco, _cep, _bairro, _cidade, _uf, _email) {
+            this.Nome = _nome;
+            this.Cpf = _cpf;
+            this.Senha = _senha;
+            this.Data_Nasc = _data_nasc;
+            this.Endereco = _endereco;
+            this.Cep = _cep;
+            this.Bairro = _bairro;
+            this.Cidade = _cidade;
+            this.Uf = _uf;
+            this.Email = _email;
+        }
+        cadastro = new Cadastro(
+	<?php foreach($dado as $_POST{ echo $dado;  ?> , <?php } ?>
+
+	    //<?php echo $nome ?>,
+            //<?php echo $cpf ?>,
+            //<?php echo $senha ?>,
+            //<?php echo $data_nasc ?>,
+            //<?php echo $endereco ?>,
+            //<?php echo $cep ?>,
+            //<?php echo $bairro ?>,
+            //<?php echo $cidade ?>,
+            //<?php echo $uf ?>,
+            //<?php echo $email ?>
+        )
+	localStorage.cadastro = cadastro
+        console.log("Cadastro: " + cadastro)
+    </script>
+</head>
+
+<body></body>
+
+</html>
+
